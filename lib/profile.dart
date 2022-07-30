6681ac377d93cc7ad0e837fa1788eb63cba39ed5
kumaresh2001 <kumareshbe2001@gmail.com>
2022-07-30 22:38:55 +0530

    some changes ❤️

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -17,27 +18,61 @@ class ProfilePage extends StatelessWidget {
             child: Center(
                 child: Column(children: [
               Container(
-                padding: const EdgeInsets.only(top: 150, left: 0),
-                child: Container(
-                    padding: const EdgeInsets.only(top: 0),
-                    child: Image.asset(
-                      'assets/images/logo.png',
-                      scale: 1.0,
-                    )),
-              ),
+                  padding: const EdgeInsets.only(top: 175, left: 0),
+                  child: Image.asset('assets/images/avatar.png',
+                      width: 150, height: 150)),
               Container(
-                  padding: const EdgeInsets.only(top: 0, left: 20),
-                  child: const Text("Life goes on with Try Onn...",
-                      style: TextStyle(
-                          fontFamily: 'DancingScript',
-                          fontSize: 35,
-                          color: Color(0xffff9900)))),
+                  child: Row(children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 20),
+                    child: const Icon(FontAwesomeIcons.userTie,
+                        color: Color(0xffff9900))),
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 50),
+                    child: const Text(
+                      "Kumaresh",
+                      style: TextStyle(fontSize: 25),
+                    ))
+              ])),
               Container(
-                  child: SignInButton(
-                text: "Sign in with Google",
-                Buttons.GoogleDark,
-                onPressed: () {},
-              ))
+                  child: Row(children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 20),
+                    child: const Icon(FontAwesomeIcons.envelope,
+                        color: Color(0xffff9900))),
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 50),
+                    child: const Text(
+                      "abc@gmail.com",
+                      style: TextStyle(fontSize: 25),
+                    ))
+              ])),
+              Container(
+                  child: Row(children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 20),
+                    child: const Icon(FontAwesomeIcons.mobile,
+                        color: Color(0xffff9900))),
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 50),
+                    child: const Text(
+                      "11111111",
+                      style: TextStyle(fontSize: 25),
+                    ))
+              ])),
+              Container(
+                  child: Row(children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 20),
+                    child: const Icon(FontAwesomeIcons.cartArrowDown,
+                        color: Color(0xffff9900))),
+                Container(
+                    padding: const EdgeInsets.only(top: 40, left: 50),
+                    child: const Text(
+                      "Cart",
+                      style: TextStyle(fontSize: 25),
+                    ))
+              ]))
             ]))));
   }
 }
