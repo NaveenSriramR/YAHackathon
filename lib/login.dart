b53c196423b567b3778a537f3661bfed25e4086a
kumaresh2001 <kumareshbe2001@gmail.com>
2022-07-30 10:54:37 +0530

    home page changed

--- lib/login.dart
+++ lib/login.dart
@@ -17,21 +17,7 @@ class LoginPage extends StatelessWidget {
             child: Center(
                 child: Column(children: [
               Container(
-                  padding: const EdgeInsets.only(top: 60, left: 100),
-                  child: const Text("Welcome",
-                      style: TextStyle(
-                          fontFamily: 'DancingScript',
-                          fontSize: 60,
-                          color: Color.fromARGB(255, 102, 50, 76)))),
-              Container(
-                  padding: const EdgeInsets.only(top: 70, left: 20),
-                  child: const Text("Life goes on with Try Onn...",
-                      style: TextStyle(
-                          fontFamily: 'DancingScript',
-                          fontSize: 35,
-                          color: Color.fromARGB(255, 102, 50, 76)))),
-              Container(
-                padding: const EdgeInsets.only(top: 0, left: 0),
+                padding: const EdgeInsets.only(top: 150, left: 0),
                 child: Container(
                     padding: const EdgeInsets.only(top: 0),
                     child: Image.asset(
@@ -40,19 +26,18 @@ class LoginPage extends StatelessWidget {
                     )),
               ),
               Container(
-                  child: SignInButton(
-                Buttons.GoogleDark,
-                text: "Sign up with Google",
-                onPressed: () {},
-              )),
-              Container(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: const Text("Sign up now!",
+                  padding: const EdgeInsets.only(top: 0, left: 20),
+                  child: const Text("Life goes on with Try Onn...",
                       style: TextStyle(
                           fontFamily: 'DancingScript',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                          color: Color.fromARGB(255, 243, 199, 68))))
+                          fontSize: 35,
+                          color: Color.fromARGB(255, 102, 50, 76)))),
+              Container(
+                  child: SignInButton(
+                text: "Sign in with Google",
+                Buttons.GoogleDark,
+                onPressed: () {},
+              ))
             ]))));
   }
 }
